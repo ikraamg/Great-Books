c77ca9a6d443d3dd5057f8bbc8c651585c3ede1f
Ikraam <ikraamghoor@gmail.com>
2020-06-12 10:04:22 +0200

    Added Voting in DB

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,4 @@
 class Book < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
+  has_many :votes
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_135727) do
+ActiveRecord::Schema.define(version: 2020_06_12_075827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,4 +39,15 @@ ActiveRecord::Schema.define(version: 2020_06_11_135727) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "votes", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "book_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["book_id"], name: "index_votes_on_book_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
+  end
+
+  add_foreign_key "votes", "books"
+  add_foreign_key "votes", "users"
 end
