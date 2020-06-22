08bc789f9ef0ae79ea9665d5d9bf596e0b185c98
Ikraam <ikraamghoor@gmail.com>
2020-06-22 18:05:16 +0200

    Moved books controller methods to model scopes

--- app/models/book.rb
+++ app/models/book.rb
@@ -4,8 +4,17 @@ class Book < ApplicationRecord
   has_many :books_categories, dependent: :destroy
   has_many :categories, through: :books_categories
   has_one_attached :photo
-  default_scope { order(created_at: :desc) }
+  
   validates :author_id, presence: true
   validates :title, presence: true
   validates :text, presence: true
+
+  # default_scope { order(created_at: :desc) }
+
+  scope :order_by_newest, -> { order(created_at: :desc) }
+  scope :with_photo, -> { includes(photo_attachment: :blob) }
+  scope :with_user, -> { includes(:user) }
+  scope :with_categories, -> { includes(:categories) }
+  scope :created_by, ->(the_user) { where(author_id: the_user.id) }
+
 end
