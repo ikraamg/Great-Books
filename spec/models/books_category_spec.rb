089e26cf1da299990bfff711c08670c614422933
Ikraam <ikraamghoor@gmail.com>
2020-06-23 13:38:26 +0200

    Added validation test

--- spec/models/books_category_spec.rb
+++ spec/models/books_category_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 RSpec.describe BooksCategory, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:category_id) }
+    it 'validates_presence_of_name' do
+    bc = BooksCategory.new(book_id:nil, category_id:1)
+    bc.valid?
+    expect(bc.errors[:book].size).to eq(1)
+end
   end
 
   describe 'associations' do
